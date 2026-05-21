a10baf5230db691ebb222dea8daa655b88ada479
RVX <ubemazon@gmail.com>
2026-05-21 16:41:03 +1000

    Revert "feat: pyTREMOR_lights01 - live volcanic seismic data driving 8 LEDs via ObsPy/FDSN"
    This reverts commit 5f3b11062ca2f601c07583b815cecf8efee6745f.

--- run_tremor.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-pkill -f pyTREMOR_lights01 2>/dev/null
-pkill -f UNDERWATER_LIGTHING_TREMOR 2>/dev/null
-sleep 2
-nohup setsid /home/sjc1/venv_tremor/bin/python3 /home/sjc1/pyTREMOR_lights01.py >/tmp/tremor.log 2>&1 </dev/null &
-disown
-echo "launched pid=$!"
